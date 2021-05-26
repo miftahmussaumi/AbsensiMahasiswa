238d98793168dd4048036e983c700c8aebaeedfc
miftahmussaumi <73458428+miftahmussaumi@users.noreply.github.com>
2021-05-26 17:05:03 +0700

    ubah length pass

--- tb_pweb (2).sql
+++ tb_pweb (2).sql
@@ -114,7 +114,7 @@ CREATE TABLE `mahasiswa` (
   `nama` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nim` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
