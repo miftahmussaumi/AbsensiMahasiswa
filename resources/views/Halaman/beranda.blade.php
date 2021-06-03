31e11f03c3f3971ebeea6d5f780f3388ee1df7bd
Laila Rahmatul Aufa W.P <72001831+laila461@users.noreply.github.com>
2021-06-03 09:44:56 +0700

    dashboard

--- resources/views/Halaman/beranda.blade.php
+++ resources/views/Halaman/beranda.blade.php
@@ -49,20 +49,6 @@
                 </div>
             </div>
             <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-warning">
-                    <div class="inner">
-                        <h3>{{$jml_user}}</h3>
-
-                        <p>User Registrations</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-person-add"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
             <div class="col-sm-12 invoice-col">
                 <br>
                 From
