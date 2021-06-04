de3faa556e2845d37d95927a5866e355014a7594
kevinbeslia <kevinbeslia2018@gmail.com>
2021-06-04 17:39:30 +0700

    kelas

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -25,24 +25,45 @@ class KelasController extends Controller
     //simpan
     public function store(Request $request)
     {
-        // $kelas_kode = $request->kode_kelas;
-        // $kls = DB::table('kelas') -> where('kode_kelas','=',$kelas_kode)->get();
-        // $jml = count(collect($kls));
+        $validation = $request->validate([
+            'kode_kelas' => 'required',
+            'kode_matkul' => 'required',
+            'nama_matkul' => 'required',
+            'tahun' => 'required|integer',
+            'semester' => 'required|integer',
+            'sks' => 'required|integer',
+        ],
+        [
+            'kode_kelas.required' => 'Kode Kelas Tidak Boleh Kosong',
+            'kode_matkul.required' => 'Kode MatKul Tidak Boleh Kosong',
+            'nama_matkul.required' => 'Nama MatKul Tidak Boleh Kosong',
+            'tahun.required' => 'Tahun Tidak Boleh Kosong',
+            'tahun.integer' => 'Tahun Hanya Boleh Angka',
+            'semester.required' => 'Semester Tidak Boleh Kosong',
+            'semester.integer' => 'Semester Hanya Boleh Angka',
+            'sks.required' => 'SKS Tidak Boleh Kosong',
+            'sks.integer' => 'SKS Hanya Boleh Angka'
+        ]
+        );
+
+        $kelas_kode = $request->kode_kelas;
+        $kls = DB::table('kelas') -> where('kode_kelas','=',$kelas_kode)->get();
+        $jml = count(collect($kls));
     
-        // if($jml > 0){
-        //     session()->flash('fail');
-        // } else {
-        //     Kelas::create([
-        //         'kode_kelas' => $request->kode_kelas,
-        //         'kode_matkul' => $request->kode_matkul,
-        //         'nama_matkul' => $request->nama_matkul,
-        //         'tahun' => $request->tahun,
-        //         'semester' => $request->semester,
-        //         'sks' => $request->sks
-        //     ]);
-        //     session()->flash('success');
-        // }
-        // return redirect('kelas');
+        if($jml > 0){
+            return redirect('tambah-kelas')->with('error', 'Kode Kelas sudah pernah diinputkan');
+        } else {
+            Kelas::create([
+                'kode_kelas' => $request->kode_kelas,
+                'kode_matkul' => $request->kode_matkul,
+                'nama_matkul' => $request->nama_matkul,
+                'tahun' => $request->tahun,
+                'semester' => $request->semester,
+                'sks' => $request->sks
+            ]);
+            return redirect('kelas')->with('success', 'Data Kelas Berhasil Ditambahkan');
+        }
+        
     }
     //edit
     public function edit($id)
@@ -54,6 +75,27 @@ class KelasController extends Controller
     //update
     public function update(Request $request, $id)
     {
+        $validation = $request->validate([
+            'kode_kelas' => 'required',
+            'kode_matkul' => 'required',
+            'nama_matkul' => 'required',
+            'tahun' => 'required|integer',
+            'semester' => 'required|integer',
+            'sks' => 'required|integer',
+        ],
+        [
+            'kode_kelas.required' => 'Kode Kelas Tidak Boleh Kosong',
+            'kode_matkul.required' => 'Kode MatKul Tidak Boleh Kosong',
+            'nama_matkul.required' => 'Nama MatKul Tidak Boleh Kosong',
+            'tahun.required' => 'Tahun Tidak Boleh Kosong',
+            'tahun.integer' => 'Tahun Hanya Boleh Angka',
+            'semester.required' => 'Semester Tidak Boleh Kosong',
+            'semester.integer' => 'Semester Hanya Boleh Angka',
+            'sks.required' => 'SKS Tidak Boleh Kosong',
+            'sks.integer' => 'SKS Hanya Boleh Angka'
+        ]
+        );
+        
         $kelas_kode = $request->kode_kelas;
         $kls = DB::table('kelas') -> where('kode_kelas','=',$kelas_kode)->get();
         $jml = count(collect($kls));
---
+++ resources/views/Halaman/create/c-kelas.blade.php
@@ -0,0 +1,92 @@
+@extends('template.index')
+
+@section('title','Kelas')
+@section('judul_atas')
+<h1>Tambah Pertemuan</h1>
+@endsection
+
+@section('container')
+<div class="container-fluid">
+    <div class="invoice p-3 mb-3">
+        @if(session('error'))
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <h5><i class="icon fas fa-exclamation"></i> Data gagal disimpan</h5>
+            {{ session('error') }}
+        </div>
+        @endif
+    <div class="card card-primary">
+        <form action="{{route('simpan-kelas')}}" method="post">
+            {{csrf_field()}}
+            <div class="card-body">
+                <table>
+                    <tr>
+                        <td>
+                            <div class="form-group">
+                                <label>Kode Kelas</label>
+                                <input type="text" class="form-control @error('kode_kelas') is-invalid @enderror" value="{{@old('kode_kelas')}}" name="kode_kelas" placeholder="Kode Kelas">
+                                @error('kode_kelas')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </td>
+                        <td>
+                            <div class="form-group">
+                                <label>Kode Mata Kuliah</label>
+                                <input type="text" class="form-control @error('kode_matkul') is-invalid @enderror" value="{{@old('kode_matkul')}}" name="kode_matkul" placeholder="Kode MatKul">
+                                @error('kode_matkul')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </td>
+                        <td>
+                            <div class="form-group">
+                                <label>Nama Mata Kuliah</label>
+                                <input type="text" class="form-control @error('nama_matkul') is-invalid @enderror" value="{{@old('nama_matkul')}}" min="0" name="nama_matkul" placeholder="Nama Mata Kuliah">
+                                @error('nama_matkul')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div class="form-group">
+                                <label>Tahun</label>
+                                <input type="text" class="form-control @error('tahun') is-invalid @enderror" value="{{@old('tahun')}}" min="0" name="tahun" placeholder="Tahun" maxlength="4">
+                                @error('tahun')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </td>
+                        <td colspan="2">
+                            <div class="form-group">
+                                <label>Semester</label>
+                                <input type="text" class="form-control @error('semester') is-invalid @enderror" value="{{@old('semester')}}" min="0" name="semester" placeholder="Semester" maxlength="2">
+                                @error('semester')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror   
+                            </div>
+                        </td>
+                        <td colspan="2">
+                            <div class="form-group">
+                                <label>SKS</label>
+                                <input type="text" class="form-control @error('sks') is-invalid @enderror" value="{{@old('sks')}}" min="0" name="sks" placeholder="sks" maxlength="1">
+                                @error('sks')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <a href="{{url('kelas')}}" class="btn btn-primary"><- Back</a>
+                            <button type="submit" class="btn btn-success">Submit</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
