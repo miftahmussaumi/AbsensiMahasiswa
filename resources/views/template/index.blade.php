390459fbda1c9027a07670fb42785ba7524a7b51
miftahmussaumi <miftahmussaumi84@gmail.com>
2021-11-15 13:18:22 +0700

    yash

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -132,12 +132,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <div class="col-sm-6">
               @yield('judul_atas')
             </div><!-- /.col -->
-            <!-- <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active">Starter Page</li>
-              </ol>
-            </div> -->
           </div><!-- /.row -->
         </div><!-- /.container-fluid -->
       </div>
